2a34181f0f09975930fc9edd07f4e5b79ccb4d39
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-10-28 18:47:53 -0400

    add another app = pages

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'home',
     'sinalalebakhsh',
+    'pages',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('sinalalebakhsh/', include('sinalalebakhsh.urls')),
+    path('pages/', include('pages.urls')),
+
 ]
 
 
