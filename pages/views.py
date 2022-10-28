4a2061769fc4ce3ed319e87a5c56bb6b58b37899
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-10-28 18:50:19 -0400

    modified app pages

--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import  HttpResponse
 
 def say_hello(request):
-    return HttpResponse('Hello friend! :) سلام دوست عزیز')
+    return HttpResponse('All pages is here...[*]')
 
 
 
