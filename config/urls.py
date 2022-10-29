ee5a4365d014f2ba6aae01c21721f165cca1000a
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-10-29 18:35:24 -0400

    Debuging Create Home Page

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
+    path('', include('pages.urls')),
     path('sinalalebakhsh/', include('sinalalebakhsh.urls')),
-    path('pages/', include('pages.urls')),
 
 ]
 
