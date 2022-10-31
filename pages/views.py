b6879dc216a44149a81278819f03c2a98fd91b2c
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-10-31 16:23:49 -0400

    add context

--- pages/views.py
+++ pages/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 def home_view(request):
     context = {
         'Page_name': 'AcronProject-HomePage',
+        'description': 'Acron project is for everyone of Consciousness.'
     }
     return render(request, 'acron_project.html', context)
 
