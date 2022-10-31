36b631372b21a516bd94fcc6be1470383a10f7ac
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-10-31 18:19:41 -0400

    Debuging add many tings

--- pages/urls.py
+++ pages/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name="home"),
-    path('about/', views.about_view, name="about"),
-    path('contactus/', views.contactus_view, name="contactus"),
 ]
 
 
