197ffadcb8c9b2ee4cc95827ff90a334a83d8aea
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-01 09:43:26 -0400

    rewrite Manifest

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    # path('sina/', include('sina.urls')),
+
 
 ]
 
