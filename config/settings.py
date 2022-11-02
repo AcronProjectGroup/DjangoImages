9007060d57bcc573551ec4fca39a9e57aef12e0d
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-02 01:36:29 -0400

    add extend Links

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['acronproject.com', 'www.acronproject.com']
+ALLOWED_HOSTS = ['acronproject.com', 'www.acronproject.com', '127.0.0.1']
 
 
 # Application definition
