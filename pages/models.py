fea06ef26a995862db04c0afb4d3903f8d075b23
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-02 01:43:10 -0400

    add 1 model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    text = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+
+
+
