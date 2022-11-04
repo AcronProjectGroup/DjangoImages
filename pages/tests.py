3e5078bdfaca23a5e8010666fa1d657ba3a4b72c
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-04 15:17:24 -0400

    add another page

--- pages/tests.py
+++ pages/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.shortcuts import reverse
 
 
-class NoteListViewTest(TestCase):
+class UrlsLinksTest(TestCase):
     def test_homepage_view_by_url_static(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
@@ -12,7 +12,23 @@ class NoteListViewTest(TestCase):
         response = self.client.get(reverse('homepage'))
         self.assertEqual(response.status_code, 200)
         
-    def test_consciousness_view(self):
+    def test_consciousness_view_by_url_static(self):
         response = self.client.get('/consciousness/')
         self.assertEqual(response.status_code, 200)
+
+    def test_consciousness_view_by_name(self):
+        response = self.client.get(reverse('consciousness'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_manifest_consciousness_view_by_url_static(self):
+        response = self.client.get('/manifest-consciousness/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_manifest_consciousness_view_by_name(self):
+        response = self.client.get(reverse('manifest-consciousness'))
+        self.assertEqual(response.status_code, 200)
+
+
+
+
 
