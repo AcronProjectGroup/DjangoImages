530caf7319b0cea20d686d69e4a99f4e2c83d521
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-04 12:56:27 -0400

    Debuging 1.1.1

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.shortcuts import reverse
+
+
+class NoteListViewTest(TestCase):
+    def test_homepage_view_by_url_static(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_view_by_name(self):
+        response = self.client.get(reverse('homepage'))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_consciousness_view(self):
+        response = self.client.get('/consciousness/')
+        self.assertEqual(response.status_code, 200)
+
--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home_view, name="home"),
+    path('', views.home_view, name="homepage"),
     path('consciousness/', views.consciousness, name="consciousness"),
     path('manifest-consciousness/', views.manifest_consciousness, name="manifest-consciousness"),
 ]
