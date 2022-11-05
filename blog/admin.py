b7c878ee587f628406c7117ff936416b92ef64bf
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-05 14:40:47 -0400

    add PostAdmin customize

--- blog/admin.py
+++ blog/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 
 from .models import Post
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status')
 
-admin.site.register(Post)
+
+
+admin.site.register(Post, PostAdmin)
 
 
 
