55c399b8cc89ae6573c370f8741ee58b2c943a85
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-05 14:06:58 -0400

    Create model Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+
+    STATUS_CHOICES = (
+        ('pub', 'Published'),
+        ('drf', 'Draft'),
+    )
+    # photo = models.ImageField()
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    date_time_created = models.DateTimeField(auto_now_add=True)
+    date_time_modified = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=3)
