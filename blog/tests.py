4ba440ab995e203b2a9ae26ec9f15d0de13baf69
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-12 09:18:36 -0500

    add test

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.shortcuts import reverse
 
-# Create your tests here.
+from .models import Post
+
+class BlogPostTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='Sina')
+        Post.objects.create(
+            title  = 'Post Title',
+            author = self.user,
+            text =   'Some Test',
+            status = Post.STATUS_CHOICES[0],
+        )
+    
+    def test_blog_view(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_blog_view_by_name(self):
+        response = self.client.get(reverse('blog'))
+        self.assertEqual(response.status_code, 200)
+        
+
+
+
