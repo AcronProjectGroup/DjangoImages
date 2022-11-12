33968681e8d2e74d1606725f87e0dad05ea710a7
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-12 15:06:16 -0500

    Debugging

--- config/settings.py
+++ config/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/static/'
+STATIC_ROOT = config("STATIC_ROOT_ADDRESS")
 
 
 # Default primary key field type
