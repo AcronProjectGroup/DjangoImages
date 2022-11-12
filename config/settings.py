5fe9d81455f0d9ddeb5bfbabbab5e9bd9873b0a5
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-12 16:06:10 -0500

    Debugging [*]

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG_SSS", cast=bool)
 
 
 ALLOWED_HOSTS = ['acronproject.com', 'www.acronproject.com', '127.0.0.1']
