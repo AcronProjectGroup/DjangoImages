d791ec7d0a36c1805cabe756da852550d6baed8c
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-19 14:23:39 -0500

    Delete others

--- blog/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-from .models import Post
-
-class PostFormModel(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['title', 'text', 'author', 'status']
