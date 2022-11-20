a2de5f8b20ccd6001a3219cb8c435826ffa5d189
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-20 11:11:22 -0500

    add field sets

--- accounts/admin.py
+++ accounts/admin.py
@@ -8,7 +8,13 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser 
+    fieldsets = UserAdmin.fieldsets + (
+                                          (None, {"fields": ('age', ),}),
+                                      )
 
+    add_fieldsets = UserAdmin.add_fieldsets + (
+                                                (None, {"fields": ('age', ),}),
+                                              ) 
 
 admin.site.register(CustomUser, CustomUserAdmin)
 
