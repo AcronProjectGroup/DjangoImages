6a3bd411cf5f5a90bf1f576a9f913fb6769aeb9e
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-23 16:55:28 -0500

    add path urls.py

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),    
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', include('pages.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls'))
 ]
