a3f407e2c9a835043b6612b2a225a92efdb61662
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-23 16:56:03 -0500

    Create new app

---
+++ pages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.views.generic.base import TemplateView
+
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name="home"),
+    path('home/', TemplateView.as_view(template_name='home.html'), name="home"),
+]
+
+
+
