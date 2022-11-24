c58a3e86b3ff0f6eb73cad24cad15b8a3036139f
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-24 07:37:13 -0500

    redirect user to home/ forever

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+def homeredirectview(request):
+    return redirect('home') 
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+
