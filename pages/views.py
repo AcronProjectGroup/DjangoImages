04e10611ac2d7c5bbd714cfd0ec93eb70efa498d
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-25 03:52:29 -0500

    debugging views.py

--- pages/views.py
+++ pages/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
 
 
-def homeredirectview(request):
-    return redirect('home') 
-
-
 class HomeView(TemplateView):
     template_name = 'home.html'
 
