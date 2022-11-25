dfa0ec0134898978c4fa0b8c4dbecfe28d23c377
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-25 03:41:18 -0500

    add test for pages 2

--- pages/tests.py
+++ pages/tests.py
@@ -6,5 +6,10 @@ class HomePageTest(TestCase):
     def test_home_page_url_by_name_reversed(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
+    
+    def test_home_page_contain_content(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, 'Home')
+    
 
 
