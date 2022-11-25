43c9f2b53ef81c0515176d5b625a1fbdbbebb02a
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-25 05:00:58 -0500

    cunfig setting.py

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    # Third party
+    'crispy_forms',
+
+
     # local
     'accounts',
     'pages',
@@ -131,9 +135,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+# Authentication settings 
 AUTH_USER_MODEL = 'accounts.CustomUser'
-
-
 LOGIN_REDIRECT_URL  = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# crispy forms settings
+CRISPY_TEMPLATE_PATH = 'bootstrap4'
+
 
