5dd8502666bdaefd591804af12cd45d848a86e37
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-27 17:44:32 -0500

    add <int:pk>/ for diagnosis detail view link

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import BookListView
+from .views import BookListView, BookDetailView
 
 urlpatterns = [
     path('', BookListView.as_view(), name="book_list_view"),
+    path('<int:pk>/', BookDetailView.as_view(), name="book_detail_view"),
     #Don't forget cama ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
 ]
 
