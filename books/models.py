669f24f4a554e4ffe77542aa8b6af2f246c170b7
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-27 13:46:11 -0500

    add model book str for calling

--- books/models.py
+++ books/models.py
@@ -4,6 +4,11 @@ from django.db import models
 class BookModel(models.Model):
     title = models.CharField(max_length=200)
     bokk_author = models.CharField(max_length=200)
-    book_content = models.TextField()
+    book_description = models.TextField()
     price = models.IntegerField(max_length=8)
+    
+
+    def __str__(self):
+        return self.title
+    
 
