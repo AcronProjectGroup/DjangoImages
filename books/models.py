bdb6020d61024d2312935cd2bd7ddcf784d0ce5c
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-27 14:04:54 -0500

    change decimal field for Price

--- books/models.py
+++ books/models.py
@@ -5,8 +5,8 @@ class BookModel(models.Model):
     title = models.CharField(max_length=200)
     bokk_author = models.CharField(max_length=200)
     book_description = models.TextField()
-    price = models.IntegerField(max_length=8)
-    
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
 
     def __str__(self):
         return self.title
