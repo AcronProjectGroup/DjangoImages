3ce2cd1a2ef370d5ee9804f28a17339a8ef30f3e
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-27 14:17:33 -0500

    add class-based view

--- books/views.py
+++ books/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import BookModel
+
+class BookListView(generic.ListView):
+    model = BookModel
+    
