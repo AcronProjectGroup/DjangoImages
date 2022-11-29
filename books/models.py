b4e563f1d900a953d3a50b6d5833c5a4ae0849ea
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-29 14:59:34 -0500

    DELET ALL

--- books/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class BookModel(models.Model):
-    title = models.CharField(max_length=200)
-    bokk_author = models.CharField(max_length=200)
-    book_content = models.TextField()
-    price = models.IntegerField(max_length=8)
-
--- pages/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-from django.views.generic.base import TemplateView
-
-
-class HomeView(TemplateView):
-    template_name = 'home.html'
-
-
