453169344380c0c66f4b1c3068c7313dad89794e
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-30 14:59:29 -0500

    Reaname to book_list

--- books/urls.py
+++ books/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import BookListView
 
 urlpatterns = [
-    path('', BookListView.as_view(), name="book_list_view"),
+    path('', BookListView.as_view(), name="book_list"),
 ]
 
 
