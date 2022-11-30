70f1d495831a4f59378785af2f55d1dcf7a0a210
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-11-30 14:43:46 -0500

    content name to description

--- books/models.py
+++ books/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    content = models.TextField()
+    description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
