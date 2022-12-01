78335aa674d67451d3bdf542e3b966e740c34c4e
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-01 10:55:00 -0500

    Create class-based view

--- books/views.py
+++ books/views.py
@@ -9,6 +9,18 @@ class BookListView(generic.ListView):
     template_name = 'books/book_list.html'
     context_object_name = 'books'
 
+
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'books/book_detail.html'
+
+
+class BookCreateView(generic.CreateView):
+    model = Book
+    fields = ['title', 'author', 'description', 'price',]
+    template_name = 'books/book_create.html'
+
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    fields = ['title', 'description', 'price',]
+    template_name = 'books/book_update.html'
