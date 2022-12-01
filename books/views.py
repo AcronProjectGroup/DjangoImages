08f1047841d28bf885968820c1fb4d90949224a3
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-01 11:18:16 -0500

    create class-based view delete

--- books/views.py
+++ books/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-
+from django.urls import reverse_lazy
 
 from .models import Book
 
@@ -24,3 +24,12 @@ class BookUpdateView(generic.UpdateView):
     model = Book
     fields = ['title', 'description', 'price',]
     template_name = 'books/book_update.html'
+
+
+class BookDeleteView(generic.DeleteView):
+    model = Book
+    template_name = 'books/book_delete.html'
+    success_url = reverse_lazy('book_list')
+
+
+
