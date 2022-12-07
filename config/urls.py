a02fffb5dba4747dea63407292afb8f62e74362d
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-07 14:00:14 -0500

    add this + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

--- config/urls.py
+++ config/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('books/', include('books.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
