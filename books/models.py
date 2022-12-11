07e1b0e0712100a9fefc4d03e974766708a3f73f
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-11 15:00:46 -0500

    Create book field ForeignKey for Comment model

--- books/models.py
+++ books/models.py
@@ -22,6 +22,7 @@ class Book(models.Model):
 
 class Comment(models.Model):
     user = models.ForeignKey(get_user_model, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete)
     text = models.TextField()
 
     datetime_created = models.DateTimeField(auto_now_add=True)
