0e1b9aa948b28d4f2d5899e30bef45a4a30a8951
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-11 14:53:23 -0500

    from django.contrib.auth import get_user_model

--- books/models.py
+++ books/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
