803a90d2448e6283bb3967d5f1c9c94d22ee0bf0
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-12 15:01:14 -0500

    Create First Decorator @@@ for this model CommentAdmin

--- books/admin.py
+++ books/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 
 from .models import Book, Comment
 
-
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('user', 'book', 'text', 'datetime_created', )
 
 
 
 admin.site.register(Book)
-admin.site.register(Comment, CommentAdmin)
+# admin.site.register(Comment, CommentAdmin)
 
 
 
