c281132a6435b55a30f129c4e4629f8c3007f3e2
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-12 14:55:49 -0500

    Create class CommentAdmin(admin.ModelAdmin) for Better Show in ADMIN Panel

--- books/admin.py
+++ books/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from .models import Book, Comment
 
 
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Book)
 admin.site.register(Comment)
 
