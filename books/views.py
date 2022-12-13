7c993747b7436d07e4e7c1d8f9084eb6ea5ea023
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-13 16:26:39 -0500

    Create     book_comments = book.comments.all() for Context

--- books/views.py
+++ books/views.py
@@ -21,7 +21,10 @@ def book_detail_view(request, pk):
     # Get book object
     book = get_object_or_404(Book, pk=pk)
 
-    return render(request, 'books/book_detail.html', {'book': book})
+    # Get book comments
+    book_comments = book.comments.all()
+
+    return render(request, 'books/book_detail.html', {'book': book, 'comments': book_comments})
 
 
 
