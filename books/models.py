f3f1be9ba3ad3a0500f768eb07e22819fdff536d
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-14 11:40:33 -0500

    Create variable for recommend book to friends

--- books/models.py
+++ books/models.py
@@ -25,7 +25,7 @@ class Comment(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='comments')
     text = models.TextField()
     is_active = models.BooleanField(default=True)
-    
+    recommend = models.BooleanField(default=True)    
 
     datetime_created = models.DateTimeField(auto_now_add=True)
 
