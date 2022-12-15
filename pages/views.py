48ace475f0f4aee5ba9f8261615b0857a13f135e
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-15 12:56:36 -0500

    Calling LoginRequiredMixin for HomeView and SinaLaleBakhshPhoto and Manifest

--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class HomeView(TemplateView):
+class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'home.html'
 
-class SinaLaleBakhshPhoto(TemplateView):
+class SinaLaleBakhshPhoto(LoginRequiredMixin, TemplateView):
     template_name = 'pages/sinalalebakhsh_photo.html'
 
 
-class Manifest(TemplateView):
+class Manifest(LoginRequiredMixin, TemplateView):
     template_name = 'pages/manifest.html'
 
 
