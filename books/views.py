099513650edc1fc70a62f7a12da5098554e4c98b
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-15 11:37:53 -0500

    Import LoginRequiredMixin in file views.py

--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Book
 from .forms import CommentForm
