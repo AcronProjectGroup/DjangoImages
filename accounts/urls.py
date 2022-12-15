f29b5f74049af868106ae2f34203762549076e1c
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-15 12:54:47 -0500

    Create Redirection Anonymous user

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 
 urlpatterns = [
+    # path('', views.RedirectToLoginCustomize.as_view(), name='redirect_to_login_customize'),
     path('signup/', views.SignUpView.as_view(), name="signup"),
 ]
 
