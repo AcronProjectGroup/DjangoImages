436e0a8c61306d8f3fd91fbf8138b3faf357eab7
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-15 13:16:34 -0500

    Import login_required to views.py for functional views

--- books/views.py
+++ books/views.py
@@ -3,6 +3,7 @@ from django.views import generic
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 from .models import Book
 from .forms import CommentForm
