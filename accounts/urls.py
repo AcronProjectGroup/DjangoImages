7e82193547b89f8116f970ec6a7974198bd49a81
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-16 11:07:00 -0500

    Create new path for redirect_to_home_customize

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    # path('', views.RedirectToLoginCustomize.as_view(), name='redirect_to_login_customize'),
+    path('', views.redirect_to_home_customize, name='redirect_to_login_customize'),
     path('signup/', views.SignUpView.as_view(), name="signup"),
 ]
 
