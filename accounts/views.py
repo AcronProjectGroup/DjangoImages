ef01c14a4977ab7b18663e058f180a0a1654cef0
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-16 11:07:54 -0500

    Create new function for redirect by redirect & reverse

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.shortcuts import redirect
 
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
@@ -10,3 +11,7 @@ class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
+
+def redirect_to_home_customize(request):
+    return redirect(reverse('home'))
+
