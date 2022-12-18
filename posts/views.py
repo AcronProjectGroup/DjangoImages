57dba87a77d27eb57301b39e5d4233af5211d2d8
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-18 15:25:25 -0500

    Add modules

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+
