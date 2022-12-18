6a1408c245dfe1ce35886f5a9f220a2f7f3ad815
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-18 15:41:15 -0500

    Be commented every codes in books app

--- books/admin.py
+++ books/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 
-from .models import Book, Comment
+# from .models import Book, Comment
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'book', 'text', 'datetime_created', )
+# @admin.register(Comment)
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'book', 'text', 'datetime_created', )
 
 
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('user', 'title', 'cover', )
+# @admin.register(Book)
+# class BookAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'title', 'cover', )
 
 # admin.site.register(Book)
 # admin.site.register(Comment, CommentAdmin)
--- books/forms.py
+++ books/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 
-from .models import Comment
+# from .models import Comment
 
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ('text', 'recommend', )
+# # class CommentForm(forms.ModelForm):
+# #     class Meta:
+# #         model = Comment
+# #         fields = ('text', 'recommend', )
 
 
