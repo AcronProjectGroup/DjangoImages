64c67421d95d2e299a4c48259e757f7b4474d020
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-19 13:46:06 -0500

    Import Post and Comment app to Django-panel-admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Comment
+
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'text', 'datetime_created', )
+
+
+@admin.register(Post)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'cover', )
+
+
+
