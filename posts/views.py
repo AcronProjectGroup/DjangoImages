ab816b134fdbb03448e95349c59ae9fa1a158e82
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-19 14:17:52 -0500

    Create Functional view for detail posts

--- posts/views.py
+++ posts/views.py
@@ -5,13 +5,43 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
 
+from .models import Post
 
 
 class PostListView(LoginRequiredMixin, generic.ListView):
-    model = Book
+    model = Post
     paginate_by = 4
     template_name = 'post/post_list.html'
     context_object_name = 'posts'
 
 
-    
+
+@login_required
+def post_detail_view(request, pk):
+    # Get post object
+    post = get_object_or_404(Post, pk=pk)
+
+    # Get post comments
+    post_comments = post.comments.all()
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.user = request.user
+
+            new_comment.save()
+            comment_form = CommentForm()
+
+    else:
+        comment_form = CommentForm()
+
+
+    return render(request, 'posts/post_detail.html', {
+        'post': post,
+        'comments': post_comments,
+        'comment_form': comment_form,    
+        }
+    )
+
