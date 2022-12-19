2f5893aa5b967131d8be6e727b454c30e9142cad
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-19 14:17:20 -0500

    Create & Import post_detail_view

--- posts/urls.py
+++ posts/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 
 from .views import (
     PostListView,
+    post_detail_view,
     
 )
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name="post_list"),
-    
+    path('<int:pk>/', post_detail_view, name="post_detail"),
 ]
 
 
