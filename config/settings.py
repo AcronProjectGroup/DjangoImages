616b55cd8933133898d9dcde9cf85e104932856b
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-20 17:12:45 -0500

    Add IP 0.0.0.0

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG_SSS", cast=bool)
 
-ALLOWED_HOSTS = ['acronproject.com','www.acronproject.com', '127.0.0.1']
+ALLOWED_HOSTS = ['acronproject.com','www.acronproject.com', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
