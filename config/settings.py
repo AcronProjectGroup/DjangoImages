849c35c4041982a89cce8ffc70b952d9db884d1f
sinalalebakhsh <lalebakhshsina@gmail.com>
2022-12-27 01:52:55 -0500

    Delete and Restructure Data-Base

--- config/settings.py
+++ config/settings.py
@@ -91,8 +91,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": 'django.db.backends.postgresql',
+        
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
