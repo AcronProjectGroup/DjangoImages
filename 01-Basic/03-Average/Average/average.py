7347b9ba7ba9ba46f507a30f2fa0a4a9a66a067f
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-13 06:15:41 -0500

    Delete get average number from hard code list

---
+++ 01-Basic/03-Average/Average/average.py
@@ -0,0 +1,8 @@
+scores = [40, 50, 66, 36, 55, 81, 99, 33, 29]
+
+result = 0
+
+for i in scores:
+    result += i
+
+print(result / len(scores))
--- U.sh
+++ U.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m " Delete gihub directory in before commit "
+git commit -m " Delete get average number from hard code list "
 git push -u origin main 
