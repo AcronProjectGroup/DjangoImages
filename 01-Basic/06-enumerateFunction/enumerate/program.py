75643021222a0e0575d0df41ccd2956a29f9f185
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-13 06:43:35 -0500

    example enumerate function +

--- 01-Basic/06-enumerateFunction/enumerate/program.py
+++ 01-Basic/06-enumerateFunction/enumerate/program.py
@@ -1,4 +1,11 @@
 names = ['Sina', 'Lina', 'Jina']
+marks = [20, 25, 30]
+ages = [44, 55, 66]
+
 
 for index, numbers in enumerate(names):
     print(index, numbers)
+
+for index, name in enumerate(names):
+    print(f'name={name}, mark={marks[index]}, age={ages[index]}')
+
--- U.sh
+++ U.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m " example enumerate function "
+git commit -m " example enumerate function + "
 git push -u origin main 
