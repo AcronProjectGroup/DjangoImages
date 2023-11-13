efa81cf8e05713a3c675448a493ca728f422a144
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-13 07:05:50 -0500

    Adding Multiplication table of numbers 1 to 1000

--- 01-Basic/07-MultiplicationTable/MultiplicationTable/program.py
+++ 01-Basic/07-MultiplicationTable/MultiplicationTable/program.py
@@ -1,4 +1,4 @@
-for i in range(1, 11):
+for i in range(1, 101):
     for j in range(1, 11):
-        print(f'{i * j:3}', end=" ")
+        print(f'{i * j:4}', end=" ")
     print()
--- U.sh
+++ U.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m " Adding Multiplication table of numbers "
+git commit -m " Adding Multiplication table of numbers 1 to 1000 "
 git push -u origin main 
