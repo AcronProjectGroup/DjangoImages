2ca406a425a67de4d21d46e36868cbba72490b72
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-13 05:46:11 -0500

    Adding Rock Paper Scissors Game

--- 01-RockPaper/RockPaperScisors/RPS.py
+++ 01-RockPaper/RockPaperScisors/RPS.py
@@ -1,4 +1,20 @@
+choices = ['r', 'p', 's']
+
+aiChoice = ''
+
 userInput = input('Rock, Paper, Scissors? (R P S) -> ')
 
-choices = ['r', 'p', 's']
+if userInput in choices:
+    if userInput == aiChoice:
+        print('Tie')
+    elif userInput == 'r' and aiChoice == 's':
+        print('You won')
+    elif userInput == 'p' and aiChoice == 'r':
+        print('You won')
+    elif userInput == 's' and aiChoice == 'p':
+        print('You won')
+    else:
+        print('You lost')
+else:
+    print('Invalid input')
 
--- U.sh
+++ U.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m " this commit not work fo me maybe it's useful for you. "
+git commit -m " Adding Rock Paper Scissors Game "
 git push -u origin main 
