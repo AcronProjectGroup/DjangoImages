84486712f583ac87963a7a77230f42f0584c0857
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-16 13:55:04 -0500

    Encryption with ord function

--- 01-Basic/10-Encryption/encryption/program.py
+++ 01-Basic/10-Encryption/encryption/program.py
@@ -2,7 +2,18 @@
 
 userInput = 'sina'
 
-for i in userInput:
-    print(ord(i), end=',')
+print(f'\n{userInput} before encryption ...\n')
 
-print()
+Encryption = []
+
+for i in userInput:
+    Encryption.append(ord(i))  
+
+print(f'{userInput} encrypted = {Encryption}.\n')
+
+
+decrypted = ''
+for i in Encryption:
+    decrypted += chr(i)
+
+print(f'{Encryption} decrypted = {decrypted}.\n')
