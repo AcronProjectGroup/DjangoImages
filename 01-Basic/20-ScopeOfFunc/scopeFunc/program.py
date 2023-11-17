225261d16276e2063825bd220f14d0ab3523eb84
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-17 10:48:03 -0500

    💠 Scope Resolution

--- 01-Basic/20-ScopeOfFunc/scopeFunc/program.py
+++ 01-Basic/20-ScopeOfFunc/scopeFunc/program.py
@@ -4,12 +4,21 @@ def PrintA():
     a = 3
     print(a)
 
-print(a)
-PrintA()
-print(a)
+# print(a)
+# PrintA()
+# print(a)
 
 # Output
     # 123
     # 3
     # 123
 
+def PrintAResolution():
+    def Print():
+        print('a')
+    
+    Print()
+
+PrintAResolution()
+# Output:
+    # a
--- U.sh
+++ U.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 Scope for Functions "
+git commit -m " 💠 Scope Resolution"
 git push -u origin main 
