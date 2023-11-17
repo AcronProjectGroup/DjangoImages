d9be7d8b153b162a938e532b4b4de5029a12bc59
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-17 09:49:07 -0500

    💠 Documentation Strings = Docstring

---
+++ 01-Basic/19-DocumentationString/Docstring/program.py
@@ -0,0 +1,13 @@
+# help(len)
+# help(print)
+
+def CalSumTwoNumbers(number1, number2):
+    """This function calculates sum of two inputs
+    example:
+        number1 + number2 = result
+        1 + 2 = 3"""
+
+    return number1 + number2
+
+help(CalSumTwoNumbers)
+
--- U.sh
+++ U.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 Triple Quoted Strings"
+git commit -m " 💠 Documentation Strings = Docstring"
 git push -u origin main 
