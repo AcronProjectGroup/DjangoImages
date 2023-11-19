90e820e1181ec08f41b349658955249642285e1b
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-19 15:08:03 -0500

    💠 Create Function with Ultimate arguments

--- 01-Basic/30-CreateFunction/MinMaxSumLen/program.py
+++ 01-Basic/30-CreateFunction/MinMaxSumLen/program.py
@@ -5,14 +5,13 @@ listOfNumbers = [90, 89, 70, 128]
 def MinMaxSumLength(*args):
     global listOfNumbers
     listOfNumbers.sort(reverse=True)
-#     MinNums = min(*args)
-#     MaxNums = max(*args)    
-#     SumNums = int
-#     for i in range(*args):
-#         SumNums += i
-#     LenNums = len(*args)
-#     return MinNums, MaxNums, SumNums, LenNums
+    if len(args) > 0:
+        MinNums = min(args)
+        MaxNums = max(args)    
+        SumNums = sum(args)
+        LenNums = len(args)
+        return MinNums, MaxNums, SumNums, LenNums
 
-# print(MinMaxSumLength(1,2,3,4,5,6))
+print(MinMaxSumLength(1,2,3,4,5,6))
 MinMaxSumLength()
 print(listOfNumbers)
