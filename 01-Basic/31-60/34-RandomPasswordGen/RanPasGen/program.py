5fff896f5ce7625bb21174825c74cad3caf70c6d
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-19 19:37:50 -0500

    💠 Random Password Generator +

--- 01-Basic/31-60/34-RandomPasswordGen/RanPasGen/program.py
+++ 01-Basic/31-60/34-RandomPasswordGen/RanPasGen/program.py
@@ -26,13 +26,24 @@ def GetYesOrNo(option, default):
 
         print('Invalide input. Please write again.')
 
+def GetUsrPassLength(option, default):
+    while True:
+        UsrInput = input(f'Enter pass Length: Enter-Default=8')
+        if UsrInput == '':
+            return default
+        if UsrInput.isdigit() and int(UsrInput) > 5 :
+            return int(UsrInput)
+        print('Invalide input. Please write specified "Number" again.')
+        
+
 def GetSetUser(Settings):
     for option, default in Settings.items():
         if option != 'length':
-            GetYesOrNo(option, default)
-
+            UsrChoices = GetYesOrNo(option, default)
+            Settings[option] = UsrChoices
+        else:
+            UsrPassLength = GetUsrPassLength(option, default)
 
 GetSetUser(Settings)
-
 print(Settings)
     
