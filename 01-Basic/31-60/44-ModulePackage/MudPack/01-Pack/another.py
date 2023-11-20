c617fdf88865b99c48539eecd3c4841acc792c81
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-20 14:50:57 -0500

    💠 44= Module and Package -- commit=1

---
+++ 01-Basic/31-60/44-ModulePackage/MudPack/01-Pack/another.py
@@ -0,0 +1,15 @@
+import program
+
+program.function1()
+
+program.function2()
+
+FirstClass = program.ClassProgram("FirstClass")
+print(FirstClass)
+# Output:
+    # This is Function 1
+    # This is Function 2
+    # This is ClassProgram Class name.
+    # This is FirstClass object.
+
+
