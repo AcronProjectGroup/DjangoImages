571eddc856ca20db5893c0960ee81a2c30838644
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-20 06:12:36 -0500

    💠 Object Oriented Programming 2

--- 01-Basic/31-60/35-OOP/OOP/program.py
+++ 01-Basic/31-60/35-OOP/OOP/program.py
@@ -1,7 +1,7 @@
 class Car:
-    def __init__(self, color):
+    def __init__(self, color, model): # This is constructor 
         self.color = color # This is an "instance attribute"
-
+        self.model = model
 
 car1 = Car("Blue") # Create a instance object
 print(car1.color)
--- U.sh
+++ U.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 Object Oriented Programming "
+git commit -m " 💠 Object Oriented Programming 2 "
 git push -u origin main 
