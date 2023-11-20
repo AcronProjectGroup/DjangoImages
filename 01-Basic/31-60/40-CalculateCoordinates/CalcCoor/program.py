a061363ecc8cec8a7ddfef0213458a0c0144de02
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-20 10:53:13 -0500

    💠 40- Calculate coordinates single line commit=3

--- 01-Basic/31-60/40-CalculateCoordinates/CalcCoor/program.py
+++ 01-Basic/31-60/40-CalculateCoordinates/CalcCoor/program.py
@@ -1,12 +1,24 @@
 class Line:
-    def __init__(self, x1, y1, x2, y2):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y1 = y1
-    
-    def __str__(self):
-        return f'Line: a=({self.x1}, {self.y1}) b=({self.x2}, {self.y2})'
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
 
-line1 = Line(1,1,3,3)
+    def __str__(self):
+        return f'Line: a=({self.a[0]}, {self.a[1]}) b=({self.b[0]}, {self.b[1]})'
+    
+    def length(self):
+        return ((self.b[1] - self.a[1])**1 + (self.b[0] - self.a[0])**2) ** 0.5
+
+    def slope(self):
+        return (self.b[1]-self.a[1]) / (self.b[0] - self.a[0])
+
+line1 = Line((1,1),(3,3))
+print(line1.length())
+
+# Output:
+    # 2.449489742783178
+
+print(line1.slope())
+# Output:
+    # 1.0
 
--- U.sh
+++ U.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 40- Calculate coordinates single line commit=1 "
+git commit -m " 💠 40- Calculate coordinates single line commit=3 "
 git push -u origin main 
