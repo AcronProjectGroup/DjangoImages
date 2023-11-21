a719e1a97043ac8748f99dd5d3cef19aba8b33ad
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-21 14:56:13 -0500

    💠 58- work with files commit=1 A1/B1

--- 01-Basic/31-60/58-WorkWithFiles/A1/B1/A1.py
+++ 01-Basic/31-60/58-WorkWithFiles/A1/B1/A1.py
@@ -1,9 +1,10 @@
+file = open("Sina.txt", "w") # w: write  --- r: read
+
 while True:
     UserTextAdding = input("write in Sina.txt: ")
     if UserTextAdding == "fin":
         break
-    file = open("Sina.txt", "w") # w: write  --- r: read
     # and Delete past data
-    file.write(UserTextAdding)
+    file.write(f"{UserTextAdding}\n")
 
 
