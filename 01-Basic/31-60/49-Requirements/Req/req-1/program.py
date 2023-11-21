53d5992e8866d96c76092000a23e4bb845ca28ad
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-21 04:14:12 -0500

    💠 Requirements.txt Definition  - commit=1

---
+++ 01-Basic/31-60/49-Requirements/Req/req-1/program.py
@@ -0,0 +1,14 @@
+# 1-
+    # pip freeze >> requirements.txt
+
+# 2-
+    # virtualenv venv
+
+# 3- 
+    # source ./venv/bin/activate
+
+# 4-
+    # pip install -r requirements.txt
+
+# Output:
+    # install everything in requirements.txt file in current virtual environment 
