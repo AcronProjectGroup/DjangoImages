9a7d8dbfe680de5141280f6a2a314050c3e76757
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-21 12:01:13 -0500

    💠 51-Exception Handling Definition  - commit=1

---
+++ 01-Basic/31-60/51-ExceptionHandling/ExpHand/ExHn-1/program.py
@@ -0,0 +1,15 @@
+print("Start")
+
+UserInput = int(input("Number? "))
+
+try:
+    print(100/UserInput)
+
+except:
+    print("Exception occured")
+
+# Input = 0
+# Output:
+    # Start
+    # Number? 
+    # Exception occured
--- U.sh
+++ U.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 50-Requirements.txt Definition  - commit=1"
+git commit -m " 💠 51-Exception Handling Definition  - commit=1"
 git push -u origin main 
