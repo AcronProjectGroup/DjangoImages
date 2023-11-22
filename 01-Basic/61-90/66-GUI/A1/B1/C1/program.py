2b4992e373011c947ed1b5eee32c091851812009
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-22 17:55:47 -0500

    💠 66- GUI stand for Graphical User Interface commit=1

--- 01-Basic/61-90/66-GUI/A1/B1/C1/program.py
+++ 01-Basic/61-90/66-GUI/A1/B1/C1/program.py
@@ -2,5 +2,25 @@ import tkinter as tk
 
 window = tk.Tk()
 
-window.mainloop()
+textVar = tk.StringVar()
 
+# Label
+text = tk.Label(
+    master=window,
+    # text="Django Web Application",
+    textvariable=textVar,
+)
+UserInput = tk.Entry(
+    master=window,
+    textvariable=textVar,
+)
+UserInput.pack()
+text.pack()
+
+text1 = tk.Label(
+    master=window,
+    text="SINA LALEHBAKHSH",
+)
+
+text1.pack()
+window.mainloop()
