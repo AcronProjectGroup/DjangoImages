75d74d1f7f38297363f87854d51437e57a54bc97
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-22 10:00:18 -0500

    💠 64- Using File manager for get avrage scores students commit=2

--- 01-Basic/61-90/64-UsingFileAvrage/A1/B1/program.py
+++ 01-Basic/61-90/64-UsingFileAvrage/A1/B1/program.py
@@ -1,10 +1,18 @@
 with open("Scores.txt", "r") as reader:
     AllData = reader.read()
     SplitData = AllData.split("\n")
-    
-    SeprationDictionary = {}
-    for i in SplitData:
-        Key, Value = i.split(" ")
-        SeprationDictionary[Key] = Value
 
-    print(SeprationDictionary)
+SeprationDictionary = {}
+for i in SplitData:
+    Key, Value = i.split(" ")
+    SeprationDictionary[Key] = Value
+
+print(SeprationDictionary)
+
+# Sina 10
+# Mina 20
+# Jina 14
+# Lina 17
+# Tina 12.5
+# Bina 16.75
+# Nina 17.05
--- U.sh
+++ U.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 64- Using File manager for get avrage scores students commit=1 "
+git commit -m " 💠 64- Using File manager for get avrage scores students commit=2 "
 git push -u origin main 
