c415964c9a3216741c276e72bb40fda7002e9c33
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-23 09:04:22 -0500

    💠 76- GUI -> Calculator commit=2

--- 01-Basic/61-90/76-CalculatorNumbers/A1/B1/program.py
+++ 01-Basic/61-90/76-CalculatorNumbers/A1/B1/program.py
@@ -10,35 +10,65 @@ LBLCaclResult = tk.Label(
     height=3,
 )
 
-btn7 = tk.Button(
-    master=window,
-    text="7",
-    command=lambda: print("7"),
-)
+# btn7 = tk.Button(
+#     master=window,
+#     text="7",
+#     command=lambda: print("7"),
+# )
 
-btn8 = tk.Button(
-    master=window,
-    text="8",
-    command=lambda: print("8"),
-)
+# btn8 = tk.Button(
+#     master=window,
+#     text="8",
+#     command=lambda: print("8"),
+# )
 
-btn9 = tk.Button(
-    master=window,
-    text="9",
-    command=lambda: print("9"),
-)
+# btn9 = tk.Button(
+#     master=window,
+#     text="9",
+#     command=lambda: print("9"),
+# )
 
-btnPlus = tk.Button(
-    master=window,
-    text="+",
-    command=lambda: print("+"),
-)
+# btnPlus = tk.Button(
+#     master=window,
+#     text="+",
+#     command=lambda: print("+"),
+# )
 
-LBLCaclResult.grid(row=0, column=0, columnspan=4)
-btn7.grid(row=1, column=0)
-btn8.grid(row=1, column=1)
-btn9.grid(row=1, column=2)
-btnPlus.grid(row=1, column=3)
+# LBLCaclResult.grid(row=0, column=0, columnspan=4)
+# btn7.grid(row=1, column=0, sticky=(E, W, N, S))
+# btn8.grid(row=1, column=1, sticky=(E, W, N, S))
+# btn9.grid(row=1, column=2, sticky=(E, W, N, S))
+# btnPlus.grid(row=1, column=3, sticky=(E, W, N, S))
+
+Keys = [
+    {
+        "text": "7",
+        "command": lambda: print("7"),
+    },
+    {
+        "text": "8",
+        "command": lambda: print("8"),
+    },
+    {
+        "text": "8",
+        "command": lambda: print("8"),
+    },
+    {
+        "text": "+",
+        "command": lambda: print("+"),
+    },
+]
+
+KeysObjects = []
+
+for data in Keys:
+    btn = tk.Button(
+        master=window,
+        text=data["text"],
+        command=data["command"],
+    )
+    KeysObjects.append(btn)
+
 
 def CloseWindow():
     """
@@ -58,15 +88,13 @@ CloseBtn = tk.Button(
 )
 
 CloseBtn.grid(
-    row=10, 
-    column=0, 
-    columnspan=10, 
+    row=10,
+    column=0,
+    columnspan=10,
     sticky=(E, W),
     padx=1,
     pady=1,
 )
 
 
-
 window.mainloop()
-
