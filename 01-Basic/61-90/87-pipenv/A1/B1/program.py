4a7949d778e0ad574cc3ae0f1340c08c43186af0
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-24 07:50:46 -0500

    💠 87- pip env

---
+++ 01-Basic/61-90/87-pipenv/A1/B1/program.py
@@ -0,0 +1,29 @@
+# before
+# python -m venv venv
+
+
+# now
+# pipenv 
+
+# Instalation:
+    # pip install pipenv
+# Or
+    # sudo apt install pipenv
+    
+
+# activation:
+    # pipenv shell
+
+# Deactivate:
+    # exit
+
+
+# Unistall Djnago Command:
+    # pipenv uninstall django
+
+# To activate this project's virtualenv, run:
+    # pipenv shell
+
+# Alternatively, run a command inside the virtualenv with:
+    # pipenv run
+    
