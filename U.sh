22af91fd288ccf1ef5ae415a76fc8620bf3661dc
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-24 15:46:01 -0500

    💠 121 >> Practice05 Django Templates | commit = 5 context = {     'page_name': 'About',     'description': 'This part is about our team.' } return render(request, 'pages/about.html', context)

--- U.sh
+++ U.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice05 Django Templates | commit = 4 "
+git commit -m " 💠 121 >> Practice05 Django Templates | commit = 5
+context = {
+    'page_name': 'About',
+    'description': 'This part is about our team.'
+}
+return render(request, 'pages/about.html', context)
+     "
 git push -u origin main 
