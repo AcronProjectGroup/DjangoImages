b56ebbc2c746790f2668f90ecc7ff2d6e0d2f484
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-24 14:53:53 -0500

    💠 121 >> 04-Four | commit = 2

--- 01-Basic/121-Django/Practice05/FunctionalView/A1/B1/pages/views.py
+++ 01-Basic/121-Django/Practice05/FunctionalView/A1/B1/pages/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home_page_view(request):
+    return HttpResponse("home_page_view")
+
+def about_page_view(request):
+    return HttpResponse("about_page_view")
+
+def contactus_page_view(request):
+    return HttpResponse("contactus_page_view")
+
