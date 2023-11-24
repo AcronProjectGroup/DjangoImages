bb397d2da62839ed5b00ea4192e6d2e80d6cffac
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-24 15:22:09 -0500

    💠 121 >> Practice05 Django Templates | commit = 4

--- U.sh
+++ U.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice05 Django Templates | commit = 3 "
+git commit -m " 💠 121 >> Practice05 Django Templates | commit = 4 "
 git push -u origin main 
