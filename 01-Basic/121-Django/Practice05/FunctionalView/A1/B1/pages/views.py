412e6b4b9f59b9873d93a0b59a9691b4c7537bb4
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-24 15:34:03 -0500

    💠 121 >> Practice05 Django Templates | commit = 4

--- 01-Basic/121-Django/Practice05/FunctionalView/A1/B1/pages/views.py
+++ 01-Basic/121-Django/Practice05/FunctionalView/A1/B1/pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page_view(request):
-    return HttpResponse("home_page_view")
+    return render(request, "home.html")
 
 def about_page_view(request):
     return render(request, "pages/about.html")
