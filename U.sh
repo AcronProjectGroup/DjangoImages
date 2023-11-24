8873880f6d0f28920d0ac8e35ceef0ca9dec1ef0
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-24 16:01:16 -0500

    💠 121 >> Practice05 Django Templates | commit = 6

--- U.sh
+++ U.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice05 Django Templates | commit = 5
-context = {
-    'page_name': 'About',
-    'description': 'This part is about our team.'
-}
-return render(request, 'pages/about.html', context)
-     "
+git commit -m " 💠 121 >> Practice05 Django Templates | commit = 6"
 git push -u origin main 
