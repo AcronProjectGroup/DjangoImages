d8401b31e7991e596afb53984a11bc02fb5f3b67
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-24 11:26:02 -0500

    💠 121 >> 02-Second | commit = 2

--- 01-Basic/121-DjnagoArchive/01-20/02-Second/A1/B1/Home/urls.py
+++ 01-Basic/121-DjnagoArchive/01-20/02-Second/A1/B1/Home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import sayHello
 
 urlpatterns = [
-    path("welcome", sayHello, name="Home")    
+    path("hi/", sayHello, name="Home")    
 ]
--- 01-Basic/121-DjnagoArchive/01-20/02-Second/A1/B1/config/urls.py
+++ 01-Basic/121-DjnagoArchive/01-20/02-Second/A1/B1/config/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("hello/", include("Home.urls")),
+    path("Hello/", include("Home.urls")),
 ]
