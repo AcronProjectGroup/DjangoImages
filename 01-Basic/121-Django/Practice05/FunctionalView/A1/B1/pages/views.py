4743ac22a142449039bd496741b18bfa75e608ee
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-24 15:44:54 -0500

    💠 121 >> Practice05 Django Templates | commit = 4

--- 01-Basic/121-Django/Practice05/FunctionalView/A1/B1/pages/views.py
+++ 01-Basic/121-Django/Practice05/FunctionalView/A1/B1/pages/views.py
@@ -5,7 +5,11 @@ def home_page_view(request):
     return render(request, "home.html")
 
 def about_page_view(request):
-    return render(request, "pages/about.html")
+    context = {
+        "page_name": "About",
+        "description": "This part is about our team."
+    }
+    return render(request, "pages/about.html", context)
 
 def contactus_page_view(request):
     return render(request, "pages/contactus.html")
