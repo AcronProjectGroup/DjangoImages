23bd576e12c7a0545835e985a303a2641d28c1f0
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-24 08:19:58 -0500

    💠 87- pip env and Commands

--- 01-Basic/61-90/87-pipenv/A1/B1/program.py
+++ 01-Basic/61-90/87-pipenv/A1/B1/program.py
@@ -26,4 +26,40 @@
 
 # Alternatively, run a command inside the virtualenv with:
     # pipenv run
+
+
+# Create requirements.txt file with pipenv >> command:
+    # pipenv requirements > requirements.txt
+
+
+# Install on the pipenv from requirements.txt file:
+    # pipenv install -r requirements.txt
+
+
+# Change python version for pipenv:
+# Attention! ->> python should installed on the OS(Operation System)
     
+    # example change to 3.6 version:
+        # pipenv --python 3.6
+
+    # example change to 3.10 version:
+        # pipenv --python 3.10
+
+
+# Find Data pipenv and location:
+    # pipenv --venv
+
+# Delete pipenv:
+    # pipenv --rm
+
+
+#  What is it is this pipenv? 
+#  Like getting Logs
+#  Command:
+    # pipenv graph
+
+
+# Is there a security problem?
+# Command:
+    # pipenv check
+
--- U.sh
+++ U.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 87- pip env "
+git commit -m " 💠 87- pip env and Commands  "
 git push -u origin main 
