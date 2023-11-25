3a476cac00864ca6a89aa87b88eb35f828057dba
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-25 14:58:32 -0500

    💠 121 >> Practice07 Image-8 url post_detail commit:12 {% url 'post_detail' post.id %} in posts buttom link this approach is better than past commit

--- U.sh
+++ U.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice07 Image-7 Link buttom conected commit:12
-Link for buttom is conected to the spesific post details "
+git commit -m " 💠 121 >> Practice07 Image-8 url post_detail commit:12
+{% url 'post_detail' post.id %}
+in posts buttom link this approach is better than past commit 
+"
 git push -u origin main 
