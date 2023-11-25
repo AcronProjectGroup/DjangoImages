df2f21a5b53b2bff69a0b2074a3dc3933236433f
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-25 02:46:59 -0500

    💠 121 >> Practice06 commit = 2 Migrations for specific App:     1- python manage.py makemigrations NameApp(e.g notes)     2- python manage.py migrate

--- U.sh
+++ U.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice06 commit = 2"
+git commit -m " 💠 121 >> Practice06 commit = 2
+Migrations for specific App:
+    1- python manage.py makemigrations NameApp(e.g notes)
+    2- python manage.py migrate
+    "
 git push -u origin main 
