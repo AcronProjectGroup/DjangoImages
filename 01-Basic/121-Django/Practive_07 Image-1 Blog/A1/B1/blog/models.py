2c2a1e5adb395507aa2e30ed5d6866c4ab346225
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-25 08:44:10 -0500

    💠 121 >> Practice06 Image-7 setUp(self) for Testing = 8

---
+++ 01-Basic/121-Django/Practive_07 Image-1 Blog/A1/B1/blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Post(models.Model):
+    # Status Should be just two case
+    STATUS_CHOICES = (
+        ("pub", "Published"),
+        ("drf", "Drafts"),
+    )
+    # title
+    title = models.CharField(max_length=100)
+    # text
+    text = models.TextField()
+    # author
+    # date
+    date_created = models.DateTimeField(auto_now_add=True)
+    # date modified
+    datatime_modified = models.DateTimeField(auto_now=True)
+    # status
+    status = models.CharField(choices=STATUS_CHOICES)
+
