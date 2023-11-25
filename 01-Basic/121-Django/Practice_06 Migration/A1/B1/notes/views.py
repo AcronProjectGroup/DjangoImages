c54768065c1eb7ba3ebe888b4b4b5adcacfe18dc
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-25 05:48:45 -0500

    💠 121 >> Practice06 commit = 3 Admin Pannel

--- 01-Basic/121-Django/Practice_06 Migration/A1/B1/notes/views.py
+++ 01-Basic/121-Django/Practice_06 Migration/A1/B1/notes/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def notes_list_view(resquest):
+    return render(resquest, "notes/notes_list.html")
