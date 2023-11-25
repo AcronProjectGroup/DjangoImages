521df0b4c93e83f5fee738f60e481643858fbd83
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-25 02:54:22 -0500

    💠 121 >> Practice06 commit = 3 Admin Pannel

--- U.sh
+++ U.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice06 commit = 2
-Migrations for specific App:
-    1- python manage.py makemigrations NameApp(e.g notes)
-    2- python manage.py migrate
-    "
+git commit -m " 💠 121 >> Practice06 commit = 3
+Admin Pannel
+"
 git push -u origin main 
