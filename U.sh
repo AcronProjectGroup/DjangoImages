6b18e0d8dfe883d3c9c59ee7571d60aa7daacf72
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-25 09:56:27 -0500

    💠 121 >> Practice07 Image-3 Customize Admin Panel commit:8 Django administration

--- U.sh
+++ U.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice07 Image-2 Foreign Key commit:8"
+git commit -m " 💠 121 >> Practice07 Image-3 Customize Admin Panel commit:8
+Django administration"
 git push -u origin main 
