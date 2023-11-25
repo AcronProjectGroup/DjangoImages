1d38f3f59daf0680aa2d771f742c63b6d6d24edd
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-25 15:10:33 -0500

    💠 121 >> Practice07 Image-9 if DoesNotExist pk way_1 try:     post = Post.objects.get(pk=pk) except ObjectDoesNotExist:     post = None

---
+++ 01-Basic/121-Django/Practice_01-30/Practive_07 Image-9 DoesNotExist  way_1/A1/B1/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.post_list_view, name="posts_list"),
+    path("<int:pk>/", views.post_detail_view , name="post_detail"),
+]
