e809cc665856d7401666aeb0677a823f975cee71
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-25 08:02:27 -0500

    💠 121 >> Practice06 Image-7 setUp(self) for Testing = 8

--- U.sh
+++ U.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice06 Image-4 Three test is Green commit = 7"
+git commit -m " 💠 121 >> Practice06 Image-7 setUp(self) for Testing = 8"
 git push -u origin main 
