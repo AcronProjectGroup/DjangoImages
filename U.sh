3cf93974f90abc33f85c9566c95e23afab185827
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-25 07:54:46 -0500

    💠 121 >> Practice06 Image-4 Three test is Green commit = 7

--- U.sh
+++ U.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice06 Image-4 Two Test is Green commit = 6"
+git commit -m " 💠 121 >> Practice06 Image-4 Three test is Green commit = 7"
 git push -u origin main 
