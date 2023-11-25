4dc6060965517350e4925b400f77e8ef5f3a68d9
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-25 17:28:45 -0500

    💠 121 >> Practice07 Image-11 Testing this App commit=1

--- U.sh
+++ U.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice07 Image-10 get_object_or_404 way_2"
+git commit -m " 💠 121 >> Practice07 Image-11 Testing this App commit=1"
 git push -u origin main 
