c2ab33b00652061a6d82e7b4b411b392e2ae2c3d
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-26 14:39:58 -0500

    💠 121 >> Practice07 Image-15 setUp and setUpTestData  commit=1

--- U.sh
+++ U.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice07 Image-14 setUp and setUpTestData  commit=1 
-setUp and setUpTestData
-setUp and setUpTestData
-setUp and setUpTestData
-
-Created Just one time, not any more.
-
+git commit -m " 💠 121 >> Practice07 Image-15 setUp and setUpTestData  commit=1 
 "
 git push -u origin main 
