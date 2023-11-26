d4a7d3580b52d513a1dacd342d2c873f0055efd7
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-26 14:25:46 -0500

    💠 121 >> Practice07 Image-14 It should not display the draft  commit=1 TDD -> Test Driver Development TDD -> Test Driver Development TDD -> Test Driver Development TDD -> Test Driver Development TDD -> Test Driver Development

--- U.sh
+++ U.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice07 Image-13 another Testing  commit=1 "
+git commit -m " 💠 121 >> Practice07 Image-14 It should not display the draft  commit=1 
+TDD -> Test Driver Development
+TDD -> Test Driver Development
+TDD -> Test Driver Development
+TDD -> Test Driver Development
+TDD -> Test Driver Development
+"
 git push -u origin main 
