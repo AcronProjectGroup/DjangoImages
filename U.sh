0bb9907fce96a90acae1ab9c461a0f7a9c8bf762
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-26 13:26:00 -0500

    💠 121 >> Practice07 Image-12 Myself Testing  commit=1     # My first testing is Greeen :)

--- U.sh
+++ U.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice07 Image-11 Testing this App commit=1"
+git commit -m " 💠 121 >> Practice07 Image-12 Myself Testing  commit=1
+    # My first testing is Greeen :)
+"
 git push -u origin main 
