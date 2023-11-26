f49f5b8fc756e79f3be1dbca8574c2761c60f628
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-26 14:02:06 -0500

    💠 121 >> Practice07 Image-13 another Testing  commit=1

--- U.sh
+++ U.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice07 Image-12 Myself Testing  commit=1
-    # My first testing is Greeen :)
-"
+git commit -m " 💠 121 >> Practice07 Image-13 another Testing  commit=1 "
 git push -u origin main 
