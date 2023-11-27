fd6c89432c3a2ad947f965dc2f71035b0975d9b3
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-27 11:35:52 -0500

    💠 121 >> Practice07 Image-21 Update Post Add Edit Button. Develop Button ATTENTION! This is before Authentication.

--- U.sh
+++ U.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice07 Image-20 DjForm as_p and as_table
-Django Form as_p and as_table example "
+git commit -m " 💠 121 >> Practice07 Image-21 Update Post
+Add Edit Button.
+Develop Button
+ATTENTION!
+This is before Authentication.
+"
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
