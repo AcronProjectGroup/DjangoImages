22c3b0145d7d6007cea1f17c2cc2370336ce2bfc
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-27 04:40:51 -0500

    💠 121 >> Practice07 Image-16 Form commit=1 This aproach have validation problems This aproach have validation problems This aproach have validation problems

--- U.sh
+++ U.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 MIT LICENSE Added "
+git commit -m " 💠 121 >> Practice07 Image-16 Form commit=1 
+This aproach have validation problems
+This aproach have validation problems
+This aproach have validation problems"
 git push -u origin main 
