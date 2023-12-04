426a0f5819a8385aa1527ac772bbbcca20c8655b
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-12-04 10:23:05 -0500

    💠 121 >> Practice09 Image01 Book Store Application

--- U.sh
+++ U.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice08 Image-03 Authentication  Signup System
-Signup System  
-Add Login button and Emojy 
-ATTENTION!
-This is before Authentication.
-"
+git commit -m " 💠 121 >> Practice09 Image01 Book Store Application"
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
