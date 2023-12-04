a62184bc4480de182cadb21dd8a252d17def6052
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-12-04 11:05:27 -0500

    💠 121 >> Practice09 Image01 Book Store Application
    UserCreationForm for SignUp
    UserChangeForm for Admin

--- 01-Basic/121-Django/Practice09 Image01 BookStore/A1/B1/accounts/forms.py
+++ 01-Basic/121-Django/Practice09 Image01 BookStore/A1/B1/accounts/forms.py
@@ -1,7 +1,20 @@
 # UserCreationForm for SignUp
 # UserChangeForm for Admin
 
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ("age", )
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
 
 
 
