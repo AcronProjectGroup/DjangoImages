a066698facc04c8f9573c94c7b285eb8132d6224
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-12-04 13:11:24 -0500

    💠 121 >> Practice09 Image02

--- 01-Basic/121-Django/Practice09 Image01 BookStore/A1/B1/config/urls.py
+++ 01-Basic/121-Django/Practice09 Image01 BookStore/A1/B1/config/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls"))
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
 
 
