f57238456c54a5a4a6384c180405f817abb03be5
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-12-04 16:05:54 -0500

    💠 121 >> Practice09 Image03  instance views

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice09 Image02 Login Page"
+git commit -m " 💠 121 >> Practice09 Image03  instance views "
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
