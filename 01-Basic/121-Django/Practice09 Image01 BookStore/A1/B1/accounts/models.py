05a0ba6568234b2a2517052d3bcb3ed97e322666
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-12-04 10:33:48 -0500

    💠 121 >> Practice09 Image01 Book Store Application

--- 01-Basic/121-Django/Practice09 Image01 BookStore/A1/B1/accounts/models.py
+++ 01-Basic/121-Django/Practice09 Image01 BookStore/A1/B1/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# You can use this for first you don't know CustomUser, what will have: 
+# class CustomUser(AbstractUser):
+#     pass
+
+
+
+class CustomUser(AbstractUser):
+    pass
+
+
