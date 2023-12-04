087c1be878a0be4f75af9374b3bcaa2f8cd4054a
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-12-04 15:56:31 -0500

    💠 121 >> Practice09 Image02 Login Page

--- 01-Basic/121-Django/Practice_01-30/Practice-09/Practice09 Image-01 BookStore Login Page/A1/B1/config/urls.py
+++ 01-Basic/121-Django/Practice_01-30/Practice-09/Practice09 Image-01 BookStore Login Page/A1/B1/config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.auth import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice09 Image02"
+git commit -m " 💠 121 >> Practice09 Image02 Login Page"
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
