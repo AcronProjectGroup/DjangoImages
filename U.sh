756f756eb187d9256b210dcfd9a9842ce66fdde8
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-10 11:22:55 -0500

    💠 121 >> Practice11 Image-1 add pages APP

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice10  Custom User model For ALL  "
+git commit -m " 💠 121 >> Practice11 Image-1 add pages APP  "
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
