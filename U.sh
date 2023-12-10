567755bc9692fcf1d716e533a08e285ce75d8d7c
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-10 13:03:17 -0500

    💠 121 >> Practice11 Image-3 another Testing

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice11 Image-2 Testing this web app  "
+git commit -m " 💠 121 >> Practice11 Image-3 another Testing "
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
