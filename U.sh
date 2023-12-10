296d2e1397f9965972a5b07a63bc1d046b294d1f
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-10 13:04:38 -0500

    💠 121 >> Practice11 Renamed

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice11 Image-3 another Testing "
+git commit -m " 💠 121 >> Practice11 Renamed "
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
