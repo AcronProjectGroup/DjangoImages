f0618c7606efd739e3320f0d474abe06c1488209
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-10 16:42:09 -0500

    💠 121 >> Practice11 Image-4 Crispy Forms

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice11 Renamed "
+git commit -m " 💠 121 >> Practice11 Image-4 Crispy Forms "
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
