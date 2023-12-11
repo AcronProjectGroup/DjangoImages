25fe783d705aa7521fa1513bd124bc6ec234ebc9
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-11 08:49:09 -0500

    💠 121 >> Practice11 Image-8 Create Book model

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice11 Image-7 Change format send to user "
+git commit -m " 💠 121 >> Practice11 Image-8 Create Book model "
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
