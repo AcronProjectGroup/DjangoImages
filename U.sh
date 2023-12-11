184bceba40fd42b6bdf46aa11f49c39c6568557c
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-11 14:23:43 -0500

    💠 121 >> Practice11 Image-11 Update view book

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice11 Image-10 Create view book"
+git commit -m " 💠 121 >> Practice11 Image-11 Update view book"
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
