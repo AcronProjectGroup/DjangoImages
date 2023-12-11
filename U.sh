8618458d42ead11de85eb9baf06729ad3f4a9483
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-11 13:10:10 -0500

    💠 121 >> Practice11 Image-9 add detail view for book list

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice11 Image-8 Create Book model "
+git commit -m " 💠 121 >> Practice11 Image-9 add detail view for book list "
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
