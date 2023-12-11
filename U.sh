b65576cc18e15cba69b8ca15d0c2e15584e58c71
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-11 08:10:13 -0500

    💠 121 >> Practice11 Image-7 Change format send to user

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice11 Image-6 Password reset when forgotten "
+git commit -m " 💠 121 >> Practice11 Image-7 Change format send to user "
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
