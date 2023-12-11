9fbf7167f33c29ecf5063af9fb994889d6a390ab
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-11 14:52:23 -0500

    💠 121 >> Practice11 Image-12 Delete view book

---
+++ 01-Basic/121-Django/Practice_01-30/Practice-11 Book Store/Image-12 Delete view book/pages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+
--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice11 Image-11 Update view book"
+git commit -m " 💠 121 >> Practice11 Image-12 Delete view book"
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
