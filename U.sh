af8eb4306ccb4c31aa9c4e633e8a3b95c67977b9
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-12 10:56:03 -0500

    💠 121 >> Practice11 Image-14 Create New Template Filters

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice11 Image-13 Template Filters"
+git commit -m " 💠 121 >> Practice11 Image-14 Create New Template Filters"
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
