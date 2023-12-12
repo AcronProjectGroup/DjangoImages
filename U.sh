a2ad6400bd8a9e605b7bafd87c1f5aae5f07fa50
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-12 14:34:02 -0500

    💠 121 >> Practice11 Image-15 The site Beautification

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice11 Image-14 Create New Template Filters"
+git commit -m " 💠 121 >> Practice11 Image-15 The site Beautification "
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
