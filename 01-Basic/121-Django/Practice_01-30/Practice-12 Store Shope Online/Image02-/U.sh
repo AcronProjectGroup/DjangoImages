c80d487d6b7e491d7f28cc69b033d592f61d1c55
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-26 09:26:08 -0500

    💠 121 >> Practice-12 > Image02- Use in All Django Project

---
+++ 01-Basic/121-Django/Practice_01-30/Practice-12 Store Shope Online/Image02-/U.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git add .
+git commit -m "💠 Debug all of these"
+git push -u origin sinaLaleh
+
+# with ScreenShot for showing demo png file
+
+
--- U.sh
+++ U.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice-12 > Image01- First Steps "
+git commit -m " 💠 121 >> Practice-12 > Image02- Use in All Django Project
+ "
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
