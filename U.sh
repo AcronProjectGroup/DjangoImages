c27d08b4a907647e5a5cfd597bd59f98912fc902
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-26 05:54:12 -0500

    💠 121 >> Practice-12 > Image01- First Steps

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice-12 Store Shope Online "
+git commit -m " 💠 121 >> Practice-12 > Image01- First Steps "
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
