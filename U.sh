81478ef642ce01a7ba78ed40e6a65dedbe2bce06
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-26 03:52:51 -0500

    💠 121 >> Practice-12 Store Shope Online

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice11 Image-22 Database "
+git commit -m " 💠 121 >> Practice-12 Store Shope Online "
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
