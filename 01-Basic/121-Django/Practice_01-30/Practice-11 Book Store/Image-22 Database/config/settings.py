935dd17f1273ecfecf363c182834b8a52f01ba06
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-26 03:26:10 -0500

    💠 121 >> Practice11 Image-22 Database

--- 01-Basic/121-Django/Practice_01-30/Practice-11 Book Store/Image-22 Database/config/settings.py
+++ 01-Basic/121-Django/Practice_01-30/Practice-11 Book Store/Image-22 Database/config/settings.py
@@ -72,13 +72,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD':'postgres',
+        'HOST': 'db',
+        'POST': 5432,
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
