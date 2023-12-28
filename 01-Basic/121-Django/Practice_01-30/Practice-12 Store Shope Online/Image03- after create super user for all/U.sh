8a0b738c345e602874362bb160f345185abfdc79
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-28 12:27:59 -0500

    💠 121 >> Image03- after create super user for all

---
+++ 01-Basic/121-Django/Practice_01-30/Practice-12 Store Shope Online/Image03- after create super user for all/U.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git add .
+git commit -m "💠 42- create new class for handle view in accounts/views.py "
+git push -u origin sinaLaleh
+
+# with ScreenShot for showing demo png file
+
+
--- U.sh
+++ U.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121 >> Practice-12 > Image02- Use in All Django Project
- "
+git commit -m " 💠 121 >> Image03- after create super user for all "
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
