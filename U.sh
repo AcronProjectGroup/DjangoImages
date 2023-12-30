f48c395f0d002e6bf3f9d8e93d5824c865c0584f
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-30 09:30:37 -0500

    💠 121-Django/Practice_01-30/Practice-13 Online Shop 1

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121-Django/Practice_01-30/Practice-13 Online Shop "
+git commit -m " 💠 121-Django/Practice_01-30/Practice-13 Online Shop 1 "
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
