bf78e923b8f5550a8575fe5ead98d1699d80ac1b
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-31 02:29:25 -0500

    💠 121-Django/Practice_01-30/Practice-13 Online Shop 3      💠💠💠 For All Projects 💠💠💠      💠💠💠 For All Projects 💠💠💠      💠💠💠 For All Projects 💠💠💠

--- U.sh
+++ U.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 121-Django/Practice_01-30/Practice-13 Online Shop 2 "
+git commit -m " 💠 121-Django/Practice_01-30/Practice-13 Online Shop 3 
+     💠💠💠 For All Projects 💠💠💠
+     💠💠💠 For All Projects 💠💠💠
+     💠💠💠 For All Projects 💠💠💠
+"
 git push -u origin main 
 
 # with ScreenShot for showing demo png file
