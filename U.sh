94be96bbcadc8c0fdbee04a426bdc7ccafc5e83c
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-01-03 14:25:31 -0500

    💠 121-Django/Practice_01-30/Practice-13 Online Shop Image-04 Django All-Auth 131- add these under all auth: config/settings.py 132- Check this link The site forces the user to enter a password once.

--- U.sh
+++ U.sh
@@ -4,6 +4,7 @@ git commit -m " 💠 121-Django/Practice_01-30/Practice-13 Online Shop Image-04 
 131- add these under all auth: config/settings.py
 132- Check this link The site forces the user to enter a password once.
 
+
 "
 git push -u origin main 
 
